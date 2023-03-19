b8cc19cf692b30465c31a52aed44072c9fddf7a9
Ubuntu <ubuntu@ip-172-31-90-237.ec2.internal>
2023-03-19 18:51:00 +0000

    nine

--- 5c-interesting.py
+++ 5c-interesting.py
@@ -9,15 +9,16 @@ def main():
     command = ['./is-interesting-helper']
 
     # Run the command and capture its output
-    output = subprocess.run(command, capture_output= True, text= True)
-
+    temp = subprocess.run(command, shell= True)
+    file1 = open(r"/home/ubuntu/Hw5c/libpng/png-test-summary.txt", "r")
+    output = file1.read()
     # Parse the output to extract the coverage value
     #x = int(output.strip())
     reg = '.*10606$'
-    str = re.findall(reg, output.stdout.decode())
+    str = re.findall(reg, output)
 
     # Print the value of x
-    print("The coverage value is",str[0][16:21])
+    print("The coverage value is",str[0][15:20])
 
 #only decrease if removing test suites
 #may have to run compoundly so that we can get the coveage for the whole subset not the individual subset
