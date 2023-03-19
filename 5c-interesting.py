ad4cc20565c197474f19731a2e25793934eb8030
Ubuntu <ubuntu@ip-172-31-90-237.ec2.internal>
2023-03-19 03:00:30 +0000

    six

--- 5c-interesting.py
+++ 5c-interesting.py
@@ -1,5 +1,6 @@
 #run 
 import subprocess
+import re
 
 def main():
     print("hello")
@@ -8,13 +9,15 @@ def main():
     command = ['./is-interesting-helper']
 
     # Run the command and capture its output
-    output = subprocess.run(command, shell= True)
+    output = subprocess.run(command, capture_output= True, text= True)
 
     # Parse the output to extract the coverage value
-    x = int(output.strip())
+    #x = int(output.strip())
+    reg = '.*10606$'
+    str = re.findall(reg, output.stdout.decode())
 
     # Print the value of x
-    print("The coverage value is",x)
+    print("The coverage value is",str[0][16:21])
 
 #only decrease if removing test suites
 #may have to run compoundly so that we can get the coveage for the whole subset not the individual subset
